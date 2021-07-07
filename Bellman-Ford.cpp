f3b0e799a13e7b741cac556ccf1d0cf3fff8f59b
ishanbhutani13 <49487043+ishanbhutani13@users.noreply.github.com>
2021-07-07 23:24:13 +0530

    Add files via upload

---
+++ Bellman-Ford.cpp
@@ -0,0 +1,41 @@
+void solve()
+{
+    vector<int> d (n, INF);
+    d[v] = 0;
+    vector<int> p (n - 1);
+    int x;
+    for (int i = 0; i < n; ++i)
+    {
+        x = -1;
+        for (int j = 0; j < m; ++j)
+            if (d[e[j].a] < INF)
+                if (d[e[j].b] > d[e[j].a] + e[j].cost)
+                {
+                    d[e[j].b] = max (-INF, d[e[j].a] + e[j].cost);
+                    p[e[j].b] = e[j].a;
+                    x = e[j].b;
+                }
+    }
+
+    if (x == -1)
+        cout << "No negative cycle from " << v;
+    else
+    {
+        int y = x;
+        for (int i = 0; i < n; ++i)
+            y = p[y];
+
+        vector<int> path;
+        for (int cur = y; ; cur = p[cur])
+        {
+            path.push_back (cur);
+            if (cur == y && path.size() > 1)
+                break;
+        }
+        reverse (path.begin(), path.end());
+
+        cout << "Negative cycle: ";
+        for (size_t i = 0; i < path.size(); ++i)
+            cout << path[i] << ' ';
+    }
+}
