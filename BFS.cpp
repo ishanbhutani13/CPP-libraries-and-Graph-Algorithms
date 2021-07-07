796f37d94c0948ceb415cb5272d8a2f93936cb25
ishanbhutani13 <49487043+ishanbhutani13@users.noreply.github.com>
2021-07-07 23:22:16 +0530

    Add files via upload

---
+++ BFS.cpp
@@ -0,0 +1,37 @@
+vector<vector<int>> adj;  // adjacency list representation
+int n; // number of nodes
+int s; // source vertex
+queue<int> q;
+vector<bool> used(n);
+vector<int> d(n), p(n);
+
+void bfs() {
+    q.push(s);
+    used[s] = true;
+    p[s] = -1;
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        for (int u : adj[v]) {
+            if (!used[u]) {
+                used[u] = true;
+                q.push(u);
+                d[u] = d[v] + 1;
+                p[u] = v;
+            }
+        }
+    }
+}
+void print_path() {
+    if (!used[u]) {
+        cout << "No path!";
+    } else {
+        vector<int> path;
+        for (int v = u; v != -1; v = p[v])
+            path.push_back(v);
+        reverse(path.begin(), path.end());
+        cout << "Path: ";
+        for (int v : path)
+            cout << v << " ";
+    }
+}
